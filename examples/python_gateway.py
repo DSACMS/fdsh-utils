7d7338985566698b9b6c20b84d73808c2a52e3c8
Neil Martinsen-Burrell <neil.martinsen-burrell@skylight.digital>
2026-08-25 15:23:54 -0500

    Start python example

---
+++ examples/python_gateway.py
@@ -0,0 +1,13 @@
+"""FDSH gateway class."""
+
+class HubGateway:
+
+    def __init__(self,
+                 base_url,
+                 client_id,
+                 client_secret,
+                 client_cert_path,
+                 client_key_path,
+                 resolve,
+                 education_enrollment_url):
+        pass
